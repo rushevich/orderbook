f6a267009aaadd6b9b0dc45e3317925c35d097c8
George Rushevich <george@rushevich.com>
2026-09-20 17:21:09 -0400

    revised strong typing system

--- include/orderbook_core/Types.hpp
+++ include/orderbook_core/Types.hpp
@@ -1,15 +1,49 @@
 #pragma once
+#include "orderbook_core/containers/IntrusiveNode.hpp"
+
+#include <compare>
+#include <concepts>
 #include <cstdint>
-
 namespace rushevich {
+
+template <typename T, typename Tag>
+    requires std::integral<T>
+struct StrongInt {
+public:
+    StrongInt() = default;
+    explicit constexpr StrongInt(T val) noexcept : _underlying { val } {}
+    explicit constexpr operator T() const noexcept { return _underlying; }
+    constexpr T value() const noexcept { return _underlying; }
+    friend auto operator<=>(StrongInt, StrongInt) noexcept = default;
+
+private:
+    T _underlying {};
+};
+
 // This corresponds to Stock Locate in the itch spec
-enum class InstrumentID : uint16_t {};
+using InstrumentID = StrongInt<uint16_t, struct InstrumentIDTag>;
+
 // This corresponds to Order Reference Number in the itch spec
-enum class OrderID : uint64_t {};
+using OrderID = StrongInt<uint64_t, struct OrderIDTag>;
 
-enum class Type : bool { buy, sell };
+enum class Side : bool { buy, sell };
 
-enum class Quantity : uint32_t {};
+using Quantity = StrongInt<uint32_t, struct QuantityTag>;
 
-enum class Price : uint32_t {};
+// enum class Price : uint32_t {};
+using Price = StrongInt<uint32_t, struct PriceTag>;
+
+struct OrderMeta : public container::IntrusiveNode<> {
+    uint64_t oid;
+    uint32_t qty;
+    uint32_t price_tick;
+    uint16_t locate_idx; // Index for orderbook array.
+    uint8_t is_bid;
+};
+
+struct PriceLevel {
+    uint32_t head_handle;
+    uint32_t tail_handle;
+    uint32_t volume;
+};
 } // namespace rushevich
