1280f3a4c458b7ac68d18976cb17eaad9798bf2c
rushevich <george@rushevich.com>
2026-09-22 09:14:37 -0400

    sync

---
+++ include/orderbook_core/book/Orchestrator.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace rushevich::book {
+// ’Orchestrates’ OrderBook objects -- owns an array of OrderBooks
+class Orchestrator {
+public:
+private:
+};
+} // namespace rushevich::book
